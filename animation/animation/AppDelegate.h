221eceae11c0243878cc933360119f0c755bacfe
Jaylan <jaylan.lu@corp.to8to.com>
2018-11-13 09:05:23 +0800

    project

---
+++ animation/animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  animation
+//
+//  Created by Jaylan on 2018/11/13.
+//  Copyright © 2018 Jaylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ animation/animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  animation
+//
+//  Created by Jaylan on 2018/11/13.
+//  Copyright © 2018 Jaylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
